73fc455b67d4e0f3bd2d3df3e92eab5668ae868d
Sahil Bhosale <sahilbhosale0808@gmail.com>
2022-01-20 20:20:13 +0530

    feat : added highscore

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let secretNumber = Math.trunc( Math.random()*20) + 1;
 
 let score = 20;
 
-
+let highscore = 0;
 
 
 document.querySelector('.check').addEventListener('click' , function(){
@@ -27,6 +27,12 @@ document.querySelector('.check').addEventListener('click' , function(){
       document.querySelector('.number').textContent = secretNumber;
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
+
+      if (score > highscore) {
+
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
 
     } else if (guess > secretNumber) {
 
